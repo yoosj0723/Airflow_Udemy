954548c1e9abc6eb3abb4cb7584b87c98d41b7b2
yoosj_mac <rund6612@gmail.com>
2024-07-21 00:11:40 +0900

    26강 minio bucket에 주식데이터.josn 넣기

--- include/stock_market/tasks.py
+++ include/stock_market/tasks.py
@@ -1,9 +1,34 @@
 from airflow.hooks.base import BaseHook 
 import requests
 import json
+from minio import Minio
+from io import BytesIO
 
 def _get_stock_prices(url, symbol):
     url = f"{url}{symbol}?metrics=high?&interval=1d&range=1y"
     api = BaseHook.get_connection('stock_api')
     response = requests.get(url, headers=api.extra_dejson['headers'])
     return json.dumps(response.json()['chart']['result'][0])
+
+def _store_prices(stock):
+    minio=BaseHook.get_connection('minio')#.extra_dejson
+    print(minio)
+    client = Minio(         #client를 통해 minio와 연결
+        endpoint=minio.extra_dejson['endpoint_url'].split('//')[1],
+        access_key=minio.login,
+        secret_key=minio.password,
+        secure=False
+    )
+    bucket_name = 'stock-market'
+    if not client.bucket_exists(bucket_name):
+        client.make_bucket(bucket_name)
+    stock = json.loads(stock)               #a문자열을 Python dictionary로 변환
+    symbol = stock['meta']['symbol']        #stock은 XComs로 갖고 와야 하므로 parmeter설정해야됨
+    data = json.dumps(stock, ensure_ascii=False).encode('utf8')
+    objw = client.put_object(
+        bucket_name=bucket_name,
+        object_name=f'{symbol}/prices.json',     #bucket에 symbol폴더 생성 후 prices.json파일 생성
+        data=BytesIO(data),
+        length=len(data)
+    )
+    return f'{objw.bucket_name}/{symbol}'
